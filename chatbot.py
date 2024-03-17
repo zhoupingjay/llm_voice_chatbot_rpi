c9b7a88933ceccdfe1f3905696901ec9d9619122
Ping Zhou <ping.a.zhou@gmail.com>
2024-03-17 00:04:01 -0700

    stream audio response sentence by sentence, replace whisper with whisper.cpp, and streamline the installation of dependencies.

--- chatbot.py
+++ chatbot.py
@@ -4,7 +4,7 @@
 # Ping Zhou
 # https://github.com/zhoupingjay/llm_voice_chatbot_rpi
 #
-# It is inspired and based on Nick Bild's "Local LLM Assistant", with some enhancements:
+# It is inspired by Nick Bild's "Local LLM Assistant", with many enhancements:
 # https://github.com/nickbild/local_llm_assistant
 
 
@@ -13,15 +13,19 @@ from openai import OpenAI
 import pyaudio
 import wave
 import whisper
-import RPi.GPIO as GPIO
 import time
 import argparse
 import numpy as np
+import subprocess
+import RPi.GPIO as GPIO
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--llm_port', default=8080, help='Local port of the LLM server.', type=int)
-parser.add_argument('--tts', default='~/piper/piper', help='TTS program to use for speaking back.', type=str)
-parser.add_argument('--tts_model', default='~/piper/en_US-amy-medium.onnx', help='TTS model to use for speaking back.', type=str)
+parser.add_argument('--tts', default='./piper/piper', help='TTS program to use for speaking back.', type=str)
+parser.add_argument('--tts_model', default='./piper/en_US-amy-medium.onnx', help='TTS model to use for speaking back.', type=str)
+parser.add_argument('--asr', default='./whisper.cpp/main', help='Path to ASR program.', type=str)
+parser.add_argument('--asr_model', default='../whisper.cpp/models/ggml-tiny.bin', help='Path to ASR model.', type=str)
+parser.add_argument('--recording', default='./recording/input.wav', help='Temporary recording file for ASR.', type=str)
 
 args = parser.parse_args()
 
@@ -29,16 +33,12 @@ BUTTON = 8
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Available models:
-# ['tiny.en', 'tiny', 'base.en', 'base', 'small.en', 'small', 'medium.en', 'medium', 'large-v1', 'large-v2', 'large-v3', 'large']
-asr_model = whisper.load_model("base")
-
 def record_wav():
     form_1 = pyaudio.paInt16
     chans = 1
     samp_rate = 16000
     chunk = 4096
-    wav_output_filename = 'input.wav'
+    wav_output_filename = args.recording
 
     audio = pyaudio.PyAudio()
 
@@ -72,20 +72,26 @@ def record_wav():
 
     return frames
 
+def speak_back(sentence: str):
+    os.system('echo "{0}" | {1} --model {2} --output-raw | aplay -r 22050 -f S16_LE -t raw -'.format(sentence, args.tts, args.tts_model))
+
 def main():
     client = OpenAI(
         base_url=f"http://127.0.0.1:{args.llm_port}/v1",
         api_key = "sk-no-key-required"
     )
 
+    print("Ready...")
     while(True):
-        print("Ready, push to talk...")
         if GPIO.input(BUTTON) == GPIO.LOW:
             # Record and transcribe the request.
             record_wav()
-            result = asr_model.transcribe("input.wav")
-            print("Transcription: {0}".format(result["text"]))
-            request = result["text"]
+            # os.system("./asr -m ../whisper.cpp/models/ggml-tiny.bin -otxt input.wav")
+            subprocess.run([args.asr, "-m", args.asr_model, "-otxt", args.recording])
+            with open(f"{args.recording}.txt", "r") as file:
+                # Read the content of the file and store it in the variable "request"
+                request = file.read()
+            print("Transcription: ", request)
 
             # Send the query to LLM.
             completion = client.chat.completions.create(
@@ -94,17 +100,39 @@ def main():
                     {"role": "system", "content": "Your name is Amy. You are an AI assistant. Your priority is helping users with their requests."},
                     {"role": "user", "content": request}
                 ],
-                # temperature = 0.7,
+                stream=True,
+                # temperature = 0.6,
             )
-
-            # Speak back the response.
-            # Remove the double quote signs from the string, so it can be passed to the shell for TTS.
-            response_quoted = completion.choices[0].message.content.replace('"', '')
             print("LLM response: ")
             print("=========================")
-            print(response_quoted)
+
+            acc = ""
+            raw = ""
+            for chunk in completion:
+                txt = chunk.choices[0].delta.content
+                raw += txt or ""
+                if txt is None:
+                    time.sleep(0.01)
+                else:
+                    acc += txt
+                    if "." in acc:
+                        sentences = acc.split(".")
+                        utterance = ".".join(sentences[:-1])
+                        utterance = utterance.replace('"', '')
+                        if len(utterance) > 4:
+                            print(utterance + ".")
+                            acc = sentences[-1].lstrip()
+                            speak_back(utterance)
+
+            if acc:
+                print(acc)
+                utterance = acc.replace('"', '')
+                speak_back(utterance)
+
             print("=========================")
-            os.system('echo "{0}" | {1} --model {2} --output-raw | aplay -r 22050 -f S16_LE -t raw -'.format(response_quoted, args.tts, args.tts_model))
+            print("==========RAW============")
+            print(raw)
+            print("=========================")
         else:
             # So we don't occupy the CPU...
             time.sleep(0.2)
